0f892846c3fc7e850beb4865c907e44a25391eca
lyricrain <lyricrain.seo@gmail.com>
2023-03-25 11:59:52 +0900

    innerpeace

---
+++ springbootstudy-innerpeace-ci-cd/src/main/java/org/kosta/myproject/controller/HomeController.java
@@ -0,0 +1,12 @@
+package org.kosta.myproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String home() {
+		return "hello springboot innerpeace aws docker jenkins github";
+	}
+}
